70f0fec90d56ca81f482f6804e3007724c8cf698
wangzhongjie <uvdream@163.com>
2020-06-18 17:14:27 +0800

    fix🐛 chdir

---
+++ api/function/chdir.ts
@@ -0,0 +1,6 @@
+/**
+ * @description: 将当前工作目录更改为指定路径
+ * @param {type} 
+ * @return: void
+ */
+Deno.chdir("D:\\code\\github\\learning-deno\\api\\function");
---
+++ api/function/chmod.ts
@@ -0,0 +1,6 @@
+/**
+ * @description: 更改指定路径的特定文件/目录的权限
+ * @param {type} 
+ * @return: 
+ */
+await Deno.chmod("C:\\Users\\91390\\Desktop\\2222", 0o666);
